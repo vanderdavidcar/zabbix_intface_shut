48186e585503212bd50bf4430e28cdb74376e2f6
vanderson <vander_davidcar@yahoo.com.br>
2023-06-01 22:43:10 -0300

    function to retrieve .csv file

--- zbx_int_down.py
+++ zbx_int_down.py
@@ -3,14 +3,13 @@ import re
 def zbx_down_intface():
     with open("zbx_problems_export.csv", "r") as f:
         reader = f.read().splitlines()
-    
+
     for row in reader:
         if "sw" in row:
-            print(f'{row}')
             pattern = re.compile(r"Down (?P<intface>\S+)[(]")
             match = pattern.search(str(row))
             ipaddr = match.group("intface")
-    
+
             if "Vlan" or "Eth" in match:
                 print(f'!\ninterface {ipaddr}')
                 print('shutdown\n')
