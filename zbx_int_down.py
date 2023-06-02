19264edeac76b22336d216c4550196009ba0540a
vanderson <vander_davidcar@yahoo.com.br>
2023-06-01 22:55:30 -0300

    function to retrieve .csv file

--- zbx_int_down.py
+++ zbx_int_down.py
@@ -4,15 +4,19 @@ def zbx_down_intface():
     with open("zbx_problems_export.csv", "r") as f:
         reader = f.read().splitlines()
 
+    print(f'\nInterfaces found on zbx_problems_export.csv file\n')
     for row in reader:
+        # Condition to find specific device hostname "zbx_problems_export.csv" file
         if "sw" in row:
+            # Regex to find only the interfaces
             pattern = re.compile(r"Down (?P<intface>\S+)[(]")
             match = pattern.search(str(row))
             ipaddr = match.group("intface")
 
+            # Condition to find Vlan ou Eth interfaces into variable
             if "Vlan" or "Eth" in match:
-                print(f'!\ninterface {ipaddr}')
-                print('shutdown\n')
+                print(f'interface {ipaddr}')
+                print('shutdown\n!')
                 ""
 zbx_down_intface()
 
